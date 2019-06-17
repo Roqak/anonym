d565894fd2b25ded35809ae10e167e079373909a
Roqak <roqak1@gmail.com>
2019-06-17 16:15:20 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const exphbs  = require('express-handlebars');
+const app = express();
+const Twit = require('twit')
+const bodyParser = require('body-parser')
+
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars')
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: false }))
+
+
+const Twitter = new Twit({
+    consumer_key:         'R6BOuu278nj6MNhO30RRcqk0V',
+    consumer_secret:      'Lm57sTNLoNAnKSA4feedWa9fv7WtUA8bezMIYVE8KeYXpnNoe5',
+    access_token:         '1929845604-vpD60mSrz0s1QvLXyuzaacvkjRtzwY05xYWtxp6',
+    access_token_secret:  'W8QQcF8j5MnOXuVrX0TuX8b9Gbs7Uaer0PZ6dV6JYxcN0',
+})
+
+app.get('/',(req,res)=>{
+    
+    res.render('home')
+})
+
+app.post("/sendtweet",(req,res)=>{
+    console.log(req.body.tweet)
+    if(req.body.tweet){
+        (async ()=>{
+            try {
+                const data = await Twitter.post('statuses/update', { status: `${req.body.tweet}` })
+                res.send(data)
+            } catch (error) {
+                res.send(error)
+            }
+           
+        })()
+    }else{
+        res.send("error")
+    }
+    
+})
+
+
+
+app.listen(5000,()=>{
+    console.log("Listening..............")
+})
