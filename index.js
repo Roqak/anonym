23d639a22d3de0a2869c31686afd2c45e49b37c0
Roqak <roqak1@gmail.com>
2019-06-19 06:51:37 +0100

    Added ussd

--- index.js
+++ index.js
@@ -14,13 +14,9 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser('keyboard cat'));
 app.use(session({ cookie: { maxAge: 60000 }}));
 app.use(flash());
-
+require('dotenv').config()
 const Twitter = new Twit({
-    consumer_key:         'R6BOuu278nj6MNhO30RRcqk0V',
-    // consumer_secret:      'Lm57sTNLoNAnKSA4feedWa9fv7WtUA8bezMIYVE8KeYXpnNoe5',
-    // access_token:         '1929845604-vpD60mSrz0s1QvLXyuzaacvkjRtzwY05xYWtxp6',
-    // access_token_secret:  'W8QQcF8j5MnOXuVrX0TuX8b9Gbs7Uaer0PZ6dV6JYxcN0',
-    // consumer_key:         process.env.consumer_key,
+    consumer_key:         process.env.consumer_key,
     consumer_secret:      process.env.consumer_secret,
     access_token:         process.env.access_token,
     access_token_secret:  process.env.access_token_secret
@@ -51,7 +47,40 @@ app.post("/sendtweet",(req,res)=>{
     
 })
 
-
+app.post('/do', (req, res) => {
+  let {sessionId, serviceCode, phoneNumber, text} = req.body
+  if (text == '') {
+    // This is the first request. Note how we start the response with CON
+    let response = `CON What would you want to check
+    1. My Account
+    2. My phone number`
+    res.send(response)
+  } else if (text == '1') {
+    // Business logic for first level response
+    let response = `CON Choose account information you want to view
+    1. Account number
+    2. Account balance`
+    res.send(response)
+  } else if (text == '2') {
+    // Business logic for first level response
+    let response = `END Your phone number is ${phoneNumber}`
+    res.send(response)
+  } else if (text == '1*1') {
+    // Business logic for first level response
+    let accountNumber = 'ACC1001'
+    // This is a terminal request. Note how we start the response with END
+    let response = `END Your account number is ${accountNumber}`
+    res.send(response)
+  } else if (text == '1*2') {
+    // This is a second level response where the user selected 1 in the first instance
+    let balance = 'NGN 10,000'
+    // This is a terminal request. Note how we start the response with END
+    let response = `END Your balance is ${balance}`
+    res.send(response)
+  } else {
+    res.status(400).send('Bad request!')
+  }
+})
 
 app.listen(port,()=>{
     console.log(`Listening..............${port}`)
